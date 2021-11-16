1123e5988894348a2ad1682abfc016dac3fc0fcd
AlvitoDwiP <94421412+AlvitoDwiP@users.noreply.github.com>
2021-11-16 10:04:47 +0700

    Create 2098_Alvito.py

---
+++ 2098_Alvito.py
@@ -0,0 +1,8 @@
+nama = "Alvito Dwinova Permana"
+nim = "212410102098"
+kelompok = "11."
+asal = "Banyuwangi, Jawa Timur"
+fakultas = "Ilmu Komputer"
+prodi = "Teknologi Informasi."
+
+print("Halo semua, nama saya", nama, "dengan NIM", nim, "dari kelompok", kelompok, "Asal saya dari", asal, "dan saya berkuliah di Universitas Jember di Fakultas", fakultas, "Prodi", prodi, "Salam kenal semua!!")
